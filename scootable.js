275b3de79894296832df89d9b07aef4adf7f0a2c
Edward Sterkin <edwardsterkin@gmail.com>
2016-04-03 19:30:47 -0700

    initial  commit

---
+++ scootable.js
@@ -0,0 +1,12 @@
+angular.module('scootable', [])
+
+.controller('mainController', function($scope,$http) {
+   $scope.sortType = 'id'; 
+   $scope.sortReverse = 'false';
+
+   $http.get('https://app.scoot.co/api/v1/scooters.json').
+    success(function(data, status, headers, config) {
+      $scope.scooters = data.scooters;
+    })
+  
+});
