ccf8d269819895c3514ade127ad80c6349fcec85
Eric Ren <devproren@gmail.com>
2016-08-06 20:39:58 -0700

    fixed config bug

--- backend/getScootData.js
+++ backend/getScootData.js
@@ -6,24 +6,31 @@ var config = require('config');
 var dbHost = config.get('db.host');
 var dbPort = config.get('db.port');
 
-var scootApiEndpoint = config.scootApiEndpoint;
+var scootApiEndpoint = config.get('scootApiEndpoint');
 var mongodb_uri = "mongodb://" + dbHost + ":" + dbPort;
 
-request({
-	url: scootApiEndpoint,
-	json: true
-}, function (error, response, body) {
 
-	if (!error && response.statusCode === 200) {
+var minutes = 1, the_interval = minutes * 60 * 1000;
+setInterval(function() {
+  console.log("I am doing my 1 minutes check");
 
-      // push json to mongodb
-	  // TODO check if this is UTC
-      var now = moment().unix();
 
-      MongoClient.connect(mongodb_uri, function(err, db) {
-      	var scootstats = db.collection('scootstats');
-     	scootstats.insert({ _id: now , data: body});
-      	db.close();
-      });
-  }
-});
+  request({
+	  url: scootApiEndpoint,
+	  json: true
+  }, function (error, response, body) {
+
+	  if (!error && response.statusCode === 200) {
+
+	// push json to mongodb
+	    // TODO check if this is UTC
+	var now = moment().unix();
+
+	MongoClient.connect(mongodb_uri, function(err, db) {
+	  var scootstats = db.collection('scootstats');
+	  scootstats.insert({ _id: now , data: body});
+	  db.close();
+	});
+    }
+  });
+}, the_interval);
