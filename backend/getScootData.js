5db8888ac42647c5bdd507c043b0d37ce94f7b67
Eric Ren <devproren@gmail.com>
2016-10-15 16:10:19 -0700

    added backend changes

--- backend/getScootData.js
+++ backend/getScootData.js
@@ -11,26 +11,30 @@ var mongodb_uri = "mongodb://" + dbHost + ":" + dbPort;
 
 
 var minutes = 1, the_interval = minutes * 60 * 1000;
-setInterval(function() {
-  console.log("I am doing my 1 minutes check");
+setInterval(function () {
+    var ts_str = (new Date()).toUTCString();
+    console.log(ts_str);
 
 
-  request({
-	  url: scootApiEndpoint,
-	  json: true
-  }, function (error, response, body) {
+    request({
+        url: scootApiEndpoint,
+        json: true
+    }, function (error, response, body) {
 
-	  if (!error && response.statusCode === 200) {
+        if (!error && response.statusCode === 200) {
 
-	// push json to mongodb
-	    // TODO check if this is UTC
-	var now = moment().unix();
+            // push json to mongodb
+            // TODO check if this is UTC
+            var now = moment().unix();
 
-	MongoClient.connect(mongodb_uri, function(err, db) {
-	  var scootstats = db.collection('scootstats');
-	  scootstats.insert({ _id: now , data: body});
-	  db.close();
-	});
-    }
-  });
+            MongoClient.connect(mongodb_uri, function (err, db) {
+                var scootstats = db.collection('scootstats');
+                scootstats.insert({_id: now, data: body});
+                db.close();
+            });
+        } else {
+            console.log(error);
+            console.log(response.statusCode);
+        }
+    });
 }, the_interval);
