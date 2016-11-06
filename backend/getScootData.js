36f6056734ba2c740bafa6896689cf2d7e20267f
Eric Ren <devproren@gmail.com>
2016-11-06 12:01:42 -0800

    ditched pools, not needed for our use case

--- backend/getScootData.js
+++ backend/getScootData.js
@@ -9,48 +9,53 @@ var pgConfig = {
   database: config.db.name,
   password: config.db.password,
   host: config.db.host,
-  port: config.db.port,
-  max: 10, // max number of clients in the pool
-  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  port: config.db.port
 };
-
-
-var pool = new pg.Pool(pgConfig);
 
 // TODO promisify everything
 
 function insertData() {
+    var client = new pg.Client(pgConfig);
     var ts_str = (new Date()).toUTCString();
-    console.log(ts_str);
-
+    console.log(ts_str + ": Called insertData()");
 
     request({
         url: scootApiEndpoint,
         json: true
     }, function (error, response, body) {
 
-        if (!error && response.statusCode === 200) {
 
-            pool.connect(function (err, client, done) {
-                if(err) {
-                    return console.error('error fetching client from pool', err);
+        if (!error && response.statusCode === 200) {
+            var ts_str = (new Date()).toUTCString();
+            console.log(ts_str + ": Retreived json from scoot api");
+
+            // connect to our database
+            client.connect(function (err) {
+                if (err) {
+                    console.log(err);
+                    throw err;
                 }
 
                 var queryStr = 'INSERT INTO snapshot (data) VALUES ($1::jsonb)';
-
+                // execute a query on our database
                 client.query(queryStr, [body], function(err, result) {
-                    //call `done()` to release the client back to the pool
-                    done();
-
-                    if(err) {
-                        return console.error('error running query', err);
+                    if (err) {
+                        console.log(err);
+                        throw err;
                     }
 
-                    console.log(ts_str + ": Inserted data");
+                    var ts_str = (new Date()).toUTCString();
+                    console.log(ts_str + ": Performed query");
                     console.log(result);
-                    //output: 1
-                });
 
+                    // disconnect the client
+                    client.end(function (err) {
+                        if (err) {
+                            console.log(err);
+                            throw err;
+                        }
+                    });
+                });
             });
 
         } else {
@@ -60,22 +65,8 @@ function insertData() {
     });
 }
 
-pool.on('error', function (err, client) {
-    // if an error is encountered by a client while it sits idle in the pool
-    // the pool itself will emit an error event with both the error and
-    // the client which emitted the original error
-    // this is a rare occurrence but can happen if there is a network partition
-    // between your application and the database, the database restarts, etc.
-    // and so you might want to handle it and at least log it out
-    console.error('idle client error', err.message, err.stack)
-})
 
-
-
-
-
-
-var minutes = 1; 
+var minutes = 1;
 var the_interval = minutes * 60 * 1000;
 
 insertData();
